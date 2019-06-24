dc3d8047940e24d2016cf3c44402a6d289d3e85f
GooDRandall <GooDRandall@yandex.ru>
2019-06-24 15:43:02 +0500

    huy

--- lib/components/model/FriendsModel.dart
+++ lib/components/model/FriendsModel.dart
@@ -3,8 +3,7 @@ class FriendsModel {
   final String email;
   final String name;
   final String profileImageUrl;
-  final String markcolor;
 
-  const FriendsModel(this.name, this.email, this.profileImageUrl, this.markcolor);
+  const FriendsModel(this.name, this.email, this.profileImageUrl);
 
 }
