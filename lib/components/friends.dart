7ed03bdeec1d6f5bc2a801c54756c0f92a6876b0
Spectren <ignatov-e@inbox.ru>
2019-06-25 14:53:29 +0500

    Ya zaebalsa

--- lib/components/friends.dart
+++ lib/components/friends.dart
@@ -8,30 +8,27 @@ class FriendsPage extends StatefulWidget {
 }
 
 class FriendsState extends State<FriendsPage> {
-
-  bool _isProgressBarShown = true;
+  bool _isProgressBarShown = false;
   List<String> _listFriends;
 
   @override
   void initState() {
     super.initState();
 
-    App.socketClient.getMyFriends().then(
-            (ServerResponse response) {
-          this._listFriends = response.data;
-        }
-    );
+    App.socketClient.getMyFriends().then((ServerResponse response) {
+      this._listFriends = response.data as List<String>;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if(this._isProgressBarShown) {
+    if (this._isProgressBarShown) {
       return new Center(child: CircularProgressIndicator());
     }
 
     return new Scaffold(
       body: new ListView.builder(
-          shrinkWrap:true,
+          shrinkWrap: true,
           padding: const EdgeInsets.all(0.0),
           itemCount: _listFriends.length,
           itemBuilder: (context, index) {
@@ -42,26 +39,32 @@ class FriendsState extends State<FriendsPage> {
               title: new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  new Text(_listFriends[index], style: new TextStyle(fontWeight: FontWeight.bold)),
+                  new Text(_listFriends[index],
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
                   new Row(
                     children: <Widget>[
-                      new CircleAvatar(backgroundColor: Colors.yellow,),
+                      new CircleAvatar(
+                        backgroundColor: Colors.yellow,
+                      ),
                       new Padding(
                         padding: EdgeInsets.only(left: 15.0),
                       ),
-                      new IconButton(icon: new Icon(Icons.block), onPressed: () {}, iconSize: 32.0,),
+                      new IconButton(
+                        icon: new Icon(Icons.block),
+                        onPressed: () {},
+                        iconSize: 32.0,
+                      ),
                     ],
                   ),
                 ],
               ),
               subtitle: new Text("huy"),
             );
-          }
-      ),
+          }),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.person_add),
-          backgroundColor: Colors.green,
-          onPressed: () {},
+        child: Icon(Icons.person_add),
+        backgroundColor: Colors.green,
+        onPressed: () {},
       ),
     );
   }
